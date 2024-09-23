a0aa34b7889723aea8509b0a0b0398779b09641a
sahidDev09 <iamsahidofficial99@gmail.com>
2024-09-23 23:23:56 +0600

    fix: about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,61 +1,59 @@
-import Image from 'next/image';
-import React from 'react';
-import pic from "/public/assets/about-blur.jpg";
-import logo from "/public/assets/learnicaNavlogo.png";
+import Image from "next/image";
+import React from "react";
+import bandImage from "/public/assets/aboutBrandimage.png";
+import googleMap from "/public/assets/google.jpg.webp";
 import me from "/public/assets/developers/numan.jpg";
-import Link from 'next/link';
+import Link from "next/link";
 import { FiLinkedin } from "react-icons/fi";
 import { ImGithub } from "react-icons/im";
 import { FaFacebook } from "react-icons/fa";
 
-
 const page = () => {
   return (
-    <div className="w-full">
-        {/* Hero Section */}
-      <section className="hero bg-blue-600 py-4 rounded-b-[70px]">
-        <div className="container mx-auto px-4 text-center">
-          <h1 className="text-3xl font-bold text-white pt-24">About Us</h1>
-          <p className="mt-4 text-lg text-blue-200">
-            Welcome to <strong>Learnica</strong>, a next-generation platform
-            designed to simplify and enhance the online learning experience for
-            both educators and students.
-          </p>
-        </div>
-      </section>
+    <div className="container mx-auto">
       {/* Top section */}
-      <div className="flex flex-col lg:flex-row justify-between gap-8 lg:gap-12 px-6 lg:px-16 mt-16 mb-12">
-        <div className="p-6 border-2 w-full lg:w-[600px] h-auto lg:h-[600px] rounded-2xl bg-slate-400">
+      <div className=" grid md:grid-cols-2 gap-10 mt-10">
+        <div className="bg-card p-10 rounded-xl">
           <h2 className="font-bold text-red-500">How it started</h2>
-          <h1 className="pt-8 text-3xl lg:text-6xl font-bold">Our Dream is <br /> Global Learning <br /> Platform</h1>
-          <p className="mt-8 lg:mt-24 mb-10 lg:mb-32 font-bold">
-            We aim to revolutionize online education by providing an easy-to-use, community-driven platform where learning is interactive, efficient, and accessible to all.
-            Our goal is to create a thriving educational ecosystem where students, teachers, and administrators collaborate to foster growth and knowledge.
+          <h1 className="pt-8 text-3xl lg:text-6xl font-bold">
+            Our Dream is <br /> Global Learning <br /> Platform
+          </h1>
+          <p className="mt-8 lg:mt-10 mb-10 lg:mb-10 text-gray-500">
+            We aim to revolutionize online education by providing an
+            easy-to-use, community-driven platform where learning is
+            interactive, efficient, and accessible to all. Our goal is to create
+            a thriving educational ecosystem where students, teachers, and
+            administrators collaborate to foster growth and knowledge.
           </p>
         </div>
 
-        <div className="flex flex-col gap-6 mt-4">
+        <div className="flex flex-col gap-5">
           {/* Image 1 */}
-          <div
-            className="w-full h-[200px] lg:w-[600px] lg:h-[270px] bg-center bg-no-repeat bg-cover rounded-2xl"
-            style={{
-              backgroundImage: "url(/assets/about-blur.jpg)",
-              backgroundSize: 'cover',
-            }}
-          >
-            <Image className="pt-28 ml-20 pl-4" src={logo} alt='' />
+          <div className=" w-full">
+            <Image
+              src={bandImage}
+              alt="bandimage"
+              width={1000}
+              className="w-full h-[300px] rounded-xl hidden md:inline"
+              height={1000}></Image>
           </div>
           {/* Image 2 */}
-          <div className="w-full h-[200px] lg:w-[600px] lg:h-[270px]">
-            <Image className="w-full h-full rounded-2xl object-cover" src={pic} alt='' />
-          </div>
+          <Image
+            src={googleMap}
+            alt="bandimage"
+            width={1000}
+            className="w-full h-[300px] rounded-xl hidden md:inline"
+            height={1000}></Image>
         </div>
       </div>
-    
 
       {/* Team Section */}
-      <h2 className="font-bold text-red-500 px-6 lg:px-16 mt-10 lg:mt-20 mb-6">How it started</h2>
-      <h1 className="px-6 lg:px-16 text-3xl lg:text-6xl font-bold">Meet Our Dedicated Team of <br /> Learnica and Innovators</h1>
+      <h2 className="font-bold text-red-500 px-6 lg:px-16 mt-10 lg:mt-20 mb-6">
+        How it started
+      </h2>
+      <h1 className="px-6 lg:px-16 text-3xl lg:text-6xl font-bold">
+        Meet Our Dedicated Team of <br /> Learnica and Innovators
+      </h1>
 
       {/* Developer Cards */}
       <div className="container mx-auto py-6">
@@ -72,11 +70,19 @@ const page = () => {
             <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-60 transition-opacity"></div>
             <div className="relative z-10 text-white opacity-0 group-hover:opacity-100 transform translate-y-10 group-hover:translate-y-0 transition-all">
               <h1 className="text-2xl font-bold">Numan Ahmod</h1>
-              <p className="mt-2 text-green-500 font-bold">A junior web developer</p>
+              <p className="mt-2 text-green-500 font-bold">
+                A junior web developer
+              </p>
               <div className="flex text-2xl mt-4 gap-6">
-                <Link href={`https://www.linkedin.com/in/numan-ahmod`}><FiLinkedin /></Link>
-                <Link href={`https://github.com/numanahmod`}><ImGithub /></Link>
-                <Link href={`https://www.facebook.com/numan12321`}><FaFacebook /></Link>
+                <Link href={`https://www.linkedin.com/in/numan-ahmod`}>
+                  <FiLinkedin />
+                </Link>
+                <Link href={`https://github.com/numanahmod`}>
+                  <ImGithub />
+                </Link>
+                <Link href={`https://www.facebook.com/numan12321`}>
+                  <FaFacebook />
+                </Link>
               </div>
             </div>
           </div>
@@ -93,11 +99,19 @@ const page = () => {
             <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-60 transition-opacity"></div>
             <div className="relative z-10 text-white opacity-0 group-hover:opacity-100 transform translate-y-10 group-hover:translate-y-0 transition-all">
               <h1 className="text-2xl font-bold">Numan Ahmod</h1>
-              <p className="mt-2 text-green-500 font-bold">A junior web developer</p>
+              <p className="mt-2 text-green-500 font-bold">
+                A junior web developer
+              </p>
               <div className="flex text-2xl mt-4 gap-6">
-                <Link href={`https://www.linkedin.com/in/numan-ahmod`}><FiLinkedin /></Link>
-                <Link href={`https://github.com/numanahmod`}><ImGithub /></Link>
-                <Link href={`https://www.facebook.com/numan12321`}><FaFacebook /></Link>
+                <Link href={`https://www.linkedin.com/in/numan-ahmod`}>
+                  <FiLinkedin />
+                </Link>
+                <Link href={`https://github.com/numanahmod`}>
+                  <ImGithub />
+                </Link>
+                <Link href={`https://www.facebook.com/numan12321`}>
+                  <FaFacebook />
+                </Link>
               </div>
             </div>
           </div>
@@ -114,11 +128,19 @@ const page = () => {
             <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-60 transition-opacity"></div>
             <div className="relative z-10 text-white opacity-0 group-hover:opacity-100 transform translate-y-10 group-hover:translate-y-0 transition-all">
               <h1 className="text-2xl font-bold">Numan Ahmod</h1>
-              <p className="mt-2 text-green-500 font-bold">A junior web developer</p>
+              <p className="mt-2 text-green-500 font-bold">
+                A junior web developer
+              </p>
               <div className="flex text-2xl mt-4 gap-6">
-                <Link href={`https://www.linkedin.com/in/numan-ahmod`}><FiLinkedin /></Link>
-                <Link href={`https://github.com/numanahmod`}><ImGithub /></Link>
-                <Link href={`https://www.facebook.com/numan12321`}><FaFacebook /></Link>
+                <Link href={`https://www.linkedin.com/in/numan-ahmod`}>
+                  <FiLinkedin />
+                </Link>
+                <Link href={`https://github.com/numanahmod`}>
+                  <ImGithub />
+                </Link>
+                <Link href={`https://www.facebook.com/numan12321`}>
+                  <FaFacebook />
+                </Link>
               </div>
             </div>
           </div>
@@ -129,18 +151,38 @@ const page = () => {
       <div className="flex flex-col lg:flex-row justify-between gap-6 px-6 lg:px-16 py-10">
         <div className="object-cover rounded-2xl w-full lg:w-[590px] h-[300px] bg-green-200 p-8">
           <h3 className="text-red-400 text-xl font-bold">Our Goal</h3>
-          <h1 className="text-2xl lg:text-4xl font-bold mt-6">Empowering Lives <br /> Through Education</h1>
-          <p className="mt-6 font-bold">Our goal is to create a thriving educational ecosystem where students, teachers, and administrators collaborate to foster growth and knowledge.</p>
+          <h1 className="text-2xl lg:text-4xl font-bold mt-6">
+            Empowering Lives <br /> Through Education
+          </h1>
+          <p className="mt-6 font-bold">
+            Our goal is to create a thriving educational ecosystem where
+            students, teachers, and administrators collaborate to foster growth
+            and knowledge.
+          </p>
         </div>
-        <div className="object-cover rounded-2xl w-full lg:w-[590px] lg:h-[300px] bg-green-200 p-8">
-          <h2 className="text-red-400 text-2xl font-bold">Why you choose us...</h2>
+        <div className="object-cover rounded-2xl w-full lg:w-[590px] lg:h-[300px]  p-8">
+          <h2 className="text-red-400 text-2xl font-bold">
+            Why you choose us...
+          </h2>
           <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 gap-6">
-            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">Learning from home</p>
-            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">Professional certificate</p>
-            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">Financial aid support</p>
-            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">Affordable courses</p>
-            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">24/7 live supports</p>
-            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">200+ free courses</p>
+            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">
+              Learning from home
+            </p>
+            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">
+              Professional certificate
+            </p>
+            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">
+              Financial aid support
+            </p>
+            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">
+              Affordable courses
+            </p>
+            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">
+              24/7 live supports
+            </p>
+            <p className="bg-white font-bold p-[2px] rounded-sm pl-2 text-slate-400">
+              200+ free courses
+            </p>
           </div>
         </div>
       </div>
