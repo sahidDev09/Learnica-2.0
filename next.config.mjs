9d78316cbf75426791473f9bcdcdd14aa5507b1b
rahatfaruk <rahatfaruk99@gmail.com>
2024-09-26 14:09:12 +0600

    fix: all remote image-url will be valid

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        port: '',
       },
+    ],
+  },
 };
 
 export default nextConfig;
 
 
-  
