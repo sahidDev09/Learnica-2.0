4bdc7ee5d6c536705c37cd9f6691ee5376e612e7
Mazed1413 <mamazed1314@gmail.com>
2024-10-03 20:24:57 +0600

    update details page

--- src/app/all-courses/[id]/page.jsx
+++ src/app/all-courses/[id]/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { FaStar } from "react-icons/fa";
 
 const page = async ({ params }) => {
   const res = await fetch(
@@ -41,7 +42,7 @@ const page = async ({ params }) => {
   return (
     <div className="min-h-screen py-10">
       <div className="container mx-auto flex flex-col-reverse lg:flex-row px-2">
-        <div className="lg:w-5/12 bg-cyan-100 p-6 rounded-xl">
+        <div className="lg:w-5/12 bg-card p-6 rounded-xl">
           {/* title */}
           <h2 className="text-2xl md:text-3xl font-semibold">{data.title}</h2>
           {/* progress */}
@@ -80,45 +81,106 @@ const page = async ({ params }) => {
           </div>
         </div>
         <div className="lg:w-7/12 lg:px-6 my-6 lg:my-0">
-          <video width="600" className="w-full rounded-xl" controls>
+          {/* <video width="600" className="w-full rounded-xl" controls>
             <source
               src="https://www.youtube.com/watch?v=kmZz0v4COpw&t=314s"
               type="video/mp4"
             />
             Your browser does not support the video tag.
-          </video>
+          </video> */}
+          <iframe
+            className="w-full rounded-xl"
+            width="600"
+            height="340"
+            src="https://www.youtube.com/embed/kmZz0v4COpw?start=314"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
           {/* tab */}
-          <div role="tablist" className="tabs tabs-bordered my-4 bg-cyan-100">
+          <div
+            role="tablist"
+            className="tabs tabs-bordered border-primary my-4 bg-card"
+          >
+            {/*--------------------------------- Overview --------------------------------*/}
             <input
               type="radio"
               name="my_tabs_1"
               className="tab mx-2 px-2"
-              aria-label="Tab 1"
+              aria-label="Overview"
               defaultChecked
             />
-            <div role="tabpanel" className="tab-content p-10 bg-white">
-              Tab content 1
-            </div>
+            <div role="tabpanel" className="tab-content py-4 bg-white">
+              <h2 className="text-lg md:text-xl font-semibold">{data.title}</h2>
+              <div className="flex gap-5 my-4">
+                <div className="text-center">
+                  <div className="flex gap-2">
+                    <span className="font-semibold text-xl">4.6</span>{" "}
+                    <FaStar className="text-xl mt-1 text-orange-600" />
+                  </div>
+                  <h4 className="text-gray-400 font-semibold">
+                    34,506 ratings
+                  </h4>
+                </div>
+                <div className="text-center">
+                  <div className="font-semibold text-xl">5,406</div>
+                  <h4 className="text-gray-400 font-semibold">Students</h4>
+                </div>
+                <div className="text-center">
+                  <div className="font-semibold text-xl">50 h</div>
+                  <h4 className="text-gray-400 font-semibold">total</h4>
+                </div>
+              </div>
 
+              <h4 className="my-4">Last update 2024</h4>
+
+              {/* author card */}
+              <h4 className="text-primary">Author</h4>
+              <div className="flex gap-2 items-center p-2 bg-card rounded-md">
+                <Image
+                  width={30}
+                  height={30}
+                  src={"/assets/developers/numan.jpg"}
+                  alt="video_thumbnail"
+                  className="rounded w-16 h-16"
+                ></Image>
+                <div className="text-start">
+                  <h2 className="text-lg md:text-xl font-semibold">Jhon doe</h2>
+                  <h4 className="text-gray-500">Web Developer </h4>
+                </div>
+              </div>
+            </div>
+            {/* --------------------------- Q&A -------------------------*/}
             <input
               type="radio"
               name="my_tabs_1"
               className="tab mx-2 px-2"
-              aria-label="Tab 2"
+              aria-label="Q&A"
             />
             <div role="tabpanel" className="tab-content p-10 bg-white">
-              Tab content 2
+              Question and answer
             </div>
-
+            {/*------------------------------- Notes ----------------------------*/}
             <input
               type="radio"
               name="my_tabs_1"
               role="tab"
               className="tab mx-2 px-2"
-              aria-label="Tab 3"
+              aria-label="Notes"
             />
             <div role="tabpanel" className="tab-content p-10 bg-white">
-              Tab content 3
+              Notes
+            </div>
+            {/*------------------------------- Reviews --------------------------*/}
+            <input
+              type="radio"
+              name="my_tabs_1"
+              role="tab"
+              className="tab mx-2 px-2"
+              aria-label="Reviews"
+            />
+            <div role="tabpanel" className="tab-content p-10 bg-white">
+              Reviews
             </div>
           </div>
         </div>
