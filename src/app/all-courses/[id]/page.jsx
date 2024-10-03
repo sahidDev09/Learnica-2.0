9deb76bf24d0a89331975be796882e2571019d35
Mazed1413 <mamazed1314@gmail.com>
2024-10-03 16:15:52 +0600

    update course details page

--- src/app/all-courses/[id]/page.jsx
+++ src/app/all-courses/[id]/page.jsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import React from "react";
 import { MdDateRange } from "react-icons/md";
-import Comment from "../Comment";
 
 const page = async ({ params }) => {
   const res = await fetch(`http://localhost:3000/api/courses/${params.id}`);
@@ -14,63 +13,116 @@ const page = async ({ params }) => {
       </div>
     );
   }
-
+  // const { user } = useUser();
   const data = await res.json();
+  const topic = [
+    {
+      title: "Introduction to JavaScript",
+      duration: "2",
+    },
+    {
+      title: "React Basics",
+      duration: "3",
+    },
+    {
+      title: "Understanding Node.js",
+      duration: "1.5",
+    },
+    {
+      title: "MongoDB Essentials",
+      duration: "2.5",
+    },
+    {
+      title: "Advanced CSS Techniques",
+      duration: "4",
+    },
+  ];
 
   return (
     <div className="min-h-screen py-10">
-      <div className="container mx-auto px-2">
-        {/* header */}
-        <h2 className="text-2xl md:text-3xl font-semibold text-center md:w-10/12 mx-auto">
-          {data.title}
-        </h2>
+      <div className="container mx-auto flex flex-col-reverse lg:flex-row px-2">
+        <div className="lg:w-5/12 bg-cyan-100 p-6 rounded-xl">
+          {/* title */}
+          <h2 className="text-2xl md:text-3xl font-semibold">{data.title}</h2>
+          {/* progress */}
+          <div className="flex gap-2 items-center my-4">
+            <progress
+              className="progress progress-error w-56"
+              value="10"
+              max="100"
+            ></progress>
+            <span className="font-semibold">10%</span>
+          </div>
+          {/* content */}
+          <div className="space-y-3">
+            {topic.map((item, index) => (
+              <>
+                <div className="flex gap-2 items-center p-2 bg-white w-full rounded-md">
+                  <Image
+                    width={60}
+                    height={60}
+                    src={"/assets/video_thumbnail.png"}
+                    alt="video_thumbnail"
+                    className="rounded"
+                  ></Image>
+                  <div className="text-start w-full">
+                    <h2 className="text-lg md:text-xl font-semibold">
+                      {index + 1}. {item.title}
+                    </h2>
+                    <h4 className="ml-5">Duration : {item.duration} min</h4>
+                  </div>
+                  <button className="btn btn-sm bg-red-400 text-white">
+                    Play
+                  </button>
+                </div>
+              </>
+            ))}
+          </div>
+        </div>
+        <div className="lg:w-7/12 lg:px-6 my-6 lg:my-0">
+          <video width="600" className="w-full rounded-xl" controls>
+            <source
+              src="https://www.youtube.com/watch?v=kmZz0v4COpw&t=314s"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+          {/* tab */}
+          <div role="tablist" className="tabs tabs-bordered my-4 bg-cyan-100">
+            <input
+              type="radio"
+              name="my_tabs_1"
+              className="tab mx-2 px-2"
+              aria-label="Tab 1"
+              defaultChecked
+            />
+            <div role="tabpanel" className="tab-content p-10 bg-white">
+              Tab content 1
+            </div>
 
-        <div className="my-4 mt-6 flex justify-between flex-col md:flex-row">
-          <div className="flex gap-2">
-            <Image
-              alt="owner"
-              width={100}
-              height={100}
-              src={data.thumbnail}
-              className="w-16 h-16 border rounded-full"
-            ></Image>
-            <div className="flex gap-1 text-gray-600">
-              <div className="flex flex-col">
-                <span className="font-medium text-xl">{data.authorName}</span>
-                <span className="font-normal text-md">Backend Developer</span>
-              </div>
-              |
-              <span className="text-gray-600 text-xl font-medium">
-                {data.category}
-              </span>
-              |
-              <span className="flex text-gray-600 text-lg font-medium">
-                <MdDateRange className="mt-1" />
-                {data.publish_date}
-              </span>{" "}
+            <input
+              type="radio"
+              name="my_tabs_1"
+              className="tab mx-2 px-2"
+              aria-label="Tab 2"
+            />
+            <div role="tabpanel" className="tab-content p-10 bg-white">
+              Tab content 2
+            </div>
+
+            <input
+              type="radio"
+              name="my_tabs_1"
+              role="tab"
+              className="tab mx-2 px-2"
+              aria-label="Tab 3"
+            />
+            <div role="tabpanel" className="tab-content p-10 bg-white">
+              Tab content 3
             </div>
           </div>
-
-          <div className="flex flex-col text-gray-600 text-xl font-medium">
-            <span>Duration : {data.duration} h</span>
-            <span>
-              <span>Price : </span>
-              <span className="text-red-500"> {data.price}</span> $
-            </span>
-          </div>
         </div>
-        {/* body */}
-
-        <div className="">
-          <p className="py-4 md:py-8 text-lg text-gray-500">
-            {data.description}
-          </p>
-        </div>
-        {/* rating */}
       </div>
-      {/* comment/preview */}
-
-      <Comment></Comment>
     </div>
   );
 };
