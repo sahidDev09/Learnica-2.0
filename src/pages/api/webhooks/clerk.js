99f3fead6a8c9c1195392a91edf8e89f34e30efc
sahidDev09 <iamsahidofficial99@gmail.com>
2024-10-21 01:00:36 +0600

    fix

--- src/pages/api/webhooks/clerk.js
+++ src/pages/api/webhooks/clerk.js
@@ -1,5 +1,3 @@
-import { PodcastIcon } from "lucide-react";
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
