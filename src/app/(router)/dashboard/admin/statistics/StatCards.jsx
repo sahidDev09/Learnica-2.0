7f6b216fb2d795df4fda1765a18fa27647715898
rahatfaruk <rahatfaruk99@gmail.com>
2024-10-21 20:09:32 +0600

    add: StatCards UI, update statictics UI

---
+++ src/app/(router)/dashboard/admin/statistics/StatCards.jsx
@@ -0,0 +1,22 @@
+function StatCards() {
+  return (
+    <div className="flex gap-6 flex-wrap">
+      <div className="min-w-56 px-4 py-6 border-2 border-secondary rounded-md shadow-md text-center">
+        <span className="text-4xl font-bold text-secondary block">28</span>
+        <span className="text-sm text-gray-600">Course Added</span>
+      </div>
+
+      <div className="min-w-56 px-4 py-6 border-2 border-secondary rounded-md shadow-md text-center">
+        <span className="text-4xl font-bold text-secondary block">28</span>
+        <span className="text-sm text-gray-600">Course Added</span>
+      </div>
+      
+      <div className="min-w-56 px-4 py-6 border-2 border-secondary rounded-md shadow-md text-center">
+        <span className="text-4xl font-bold text-secondary block">28</span>
+        <span className="text-sm text-gray-600">Course Added</span>
+      </div>
+    </div>
+  );
+}
+
+export default StatCards;
--- src/app/(router)/dashboard/admin/statistics/page.jsx
+++ src/app/(router)/dashboard/admin/statistics/page.jsx
@@ -1,7 +1,13 @@
+import StatCards from "./StatCards";
+
 function Statistics() {
   return (  
-    <div>
-      Statistics page
+    <div className="p-4">
+      <header>
+        <h2 className="text-secondary text-2xl font-semibold mb-6 mt-6 md:mt-0">My Statistics</h2>
+      </header>
+      
+      <StatCards />
     </div>
   );
 }
