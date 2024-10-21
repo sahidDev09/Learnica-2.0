5c63a890279caf6c6aa38c127521bf9d2595aa57
rahatfaruk <rahatfaruk99@gmail.com>
2024-10-21 20:31:09 +0600

    update: statCard UI

--- src/app/(router)/dashboard/admin/statistics/StatCards.jsx
+++ src/app/(router)/dashboard/admin/statistics/StatCards.jsx
@@ -3,17 +3,17 @@ function StatCards() {
     <div className="flex gap-6 flex-wrap">
       <div className="min-w-56 px-4 py-6 border-2 border-secondary rounded-md shadow-md text-center">
         <span className="text-4xl font-bold text-secondary block">28</span>
-        <span className="text-sm text-gray-600">Course Added</span>
+        <span className="text-sm text-gray-600">Courses</span>
       </div>
 
       <div className="min-w-56 px-4 py-6 border-2 border-secondary rounded-md shadow-md text-center">
-        <span className="text-4xl font-bold text-secondary block">28</span>
-        <span className="text-sm text-gray-600">Course Added</span>
+        <span className="text-4xl font-bold text-secondary block">09</span>
+        <span className="text-sm text-gray-600">Custom Courses</span>
       </div>
-      
+
       <div className="min-w-56 px-4 py-6 border-2 border-secondary rounded-md shadow-md text-center">
-        <span className="text-4xl font-bold text-secondary block">28</span>
-        <span className="text-sm text-gray-600">Course Added</span>
+        <span className="text-4xl font-bold text-secondary block">$3000</span>
+        <span className="text-sm text-gray-600">Taka Earned</span>
       </div>
     </div>
   );
