576d7a04dcac8ff4f6dd3d74b94a705f52dbee76
sahidDev09 <iamsahidofficial99@gmail.com>
2024-10-22 19:27:15 +0600

    up: successfully update lecture title and checkbox in the local state

--- src/app/add-course/New-course/Concepts.jsx
+++ src/app/add-course/New-course/Concepts.jsx
@@ -7,29 +7,40 @@ import { Label } from "@radix-ui/react-label";
 import React, { useState } from "react";
 
 const Concepts = () => {
-  const courseLectureFormData = [
-    {
-      title: "",
-      videoUrl: "",
-      freePreview: false,
-      public_id: "",
-    },
-  ];
+  const initialLecture = {
+    title: "",
+    videoUrl: "",
+    freePreview: false,
+    public_id: "",
+  };
 
-  const [lecture, setLecture] = useState(courseLectureFormData);
+  const [lecture, setLecture] = useState([initialLecture]);
 
   const handleAddLecture = () => {
-    setLecture((prevLecture) => {
-      const updatedLecture = [...prevLecture, { ...courseLectureFormData[0] }];
-      return updatedLecture;
-    });
+    setLecture((prevLecture) => [...prevLecture, { ...initialLecture }]);
   };
+
+  const handleTitleChange = (event, index) => {
+    const updatedLectures = lecture.map((lec, i) =>
+      i === index ? { ...lec, title: event.target.value } : lec
+    );
+    setLecture(updatedLectures);
+  };
+
+  const handleFreePreviewChange = (index) => {
+    const updatedLectures = lecture.map((lec, i) =>
+      i === index ? { ...lec, freePreview: !lec.freePreview } : lec
+    );
+    setLecture(updatedLectures);
+  };
+
+  console.log(lecture, "my lecture class");
 
   return (
     <div>
       <Card>
         <CardHeader>
-          <CardTitle>Add Course Lectures {lecture.length}</CardTitle>
+          <CardTitle>Add Course Lectures ({lecture.length})</CardTitle>
         </CardHeader>
         <CardContent>
           <Button
@@ -40,16 +51,19 @@ const Concepts = () => {
           <div className=" my-2">
             {lecture.map((lec, index) => (
               <div key={index} className="border p-5 my-2 rounded-md">
-                <div className=" flex gap-5 items-center">
+                <div className="flex gap-5 items-center">
                   <h3 className=" font-semibold">Lecture {index + 1}</h3>
                   <Input
                     name={`title${index + 1}`}
                     placeholder="Write course title"
+                    value={lec.title}
                     className="max-w-96"
+                    onChange={(event) => handleTitleChange(event, index)}
                   />
                   <div className="flex items-center space-x-2">
                     <Switch
                       checked={lec.freePreview}
+                      onCheckedChange={() => handleFreePreviewChange(index)}
                       id={`freePreview${index + 1}`}
                     />
                     <Label htmlFor={`freePreview${index + 1}`}>
