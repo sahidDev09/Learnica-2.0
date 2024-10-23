1924a4058e6efb56ed0e985b83a8de582e10cc18
farha12345678 <sabikunnaharfarha01@gmail.com>
2024-10-23 21:00:59 +0600

    hmmmmmmmmmmmm

--- src/pages/api/get-orders.js
+++ src/pages/api/get-orders.js
@@ -2,23 +2,19 @@ import clientPromise from "@/lib/mongodb";
 
 export default async function handler(req, res) {
   try {
-    // Ensure a database connection is established
     const client = await clientPromise;
     const db = client.db("learnica");
     const ordersCollection = db.collection("orders");
 
-    // Handle the GET request
     if (req.method === "GET") {
       const result = await ordersCollection.find({}).toArray();
-      return res.status(200).json(result); // Set status code 200 for successful GET
+      return res.status(200).json(result); 
     } else {
-      // Handle unsupported HTTP methods
       return res
         .status(405)
         .json({ success: false, message: "Method Not Allowed" });
     }
   } catch (error) {
-    // Handle any errors during the request
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
