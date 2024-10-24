7648b4a1018cb3113caa4b648f3dc071cd80e2af
rahatfaruk <rahatfaruk99@gmail.com>
2024-10-24 22:56:47 +0600

    fix: get questions by courseId

--- src/app/all-courses/[id]/Questions.jsx
+++ src/app/all-courses/[id]/Questions.jsx
@@ -18,10 +18,10 @@ function Questions({courseId}) {
     isLoading,
     refetch,
   } = useQuery({
-    queryKey: ["qna-ques"],
+    queryKey: ["qna-ques", courseId],
     queryFn: async () => {
       const res = await fetch(
-        process.env.NEXT_PUBLIC_BASE_URL + "/api/qna-ques"
+        process.env.NEXT_PUBLIC_BASE_URL + `/api/qna-ques?courseId=${courseId}`
       );
       return res.json();
     },
