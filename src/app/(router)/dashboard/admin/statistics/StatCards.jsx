63d59568910d838ae5056d0c64a30f4fd20315f6
numanahmod <numanahmod96@gmail.com>
2024-10-25 12:19:42 +0600

    Upadate statistic

--- src/app/(router)/dashboard/admin/statistics/StatCards.jsx
+++ src/app/(router)/dashboard/admin/statistics/StatCards.jsx
@@ -13,7 +13,7 @@ function StatCards() {
 
       <div className="min-w-56 px-4 py-6 border-2 border-secondary rounded-md shadow-md text-center">
         <span className="text-4xl font-bold text-secondary block">$3000</span>
-        <span className="text-sm text-gray-600">Taka Earned</span>
+        <span className="text-sm text-gray-600">Earned</span>
       </div>
     </div>
   );
