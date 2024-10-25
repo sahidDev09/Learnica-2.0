6d1dec2dd8928355a706343b8815db09953409f6
sahidDev09 <iamsahidofficial99@gmail.com>
2024-10-25 10:02:27 +0600

    ui update: fix add course responsive

--- src/app/add-course/page.jsx
+++ src/app/add-course/page.jsx
@@ -1,17 +1,10 @@
-import AddForm from "./AddForm";
+
 import NewCourse from "./New-course/NewCourse";
 
 function AddCourse() {
   return (
     <section className="min-h-screen">
       <NewCourse />
-      {/* <div className="max-w-screen-xl mx-auto">
-        <header className="mb-6 text-center">
-          <h2 className="text-3xl md:text-4xl font-bold">Add New Course</h2>
-        </header>
-
-        <AddForm />
-      </div> */}
     </section>
   );
 }
