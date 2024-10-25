b0310daf76d1a4d7585b66f2e678761dda133f91
rahatfaruk <rahatfaruk99@gmail.com>
2024-10-25 18:36:11 +0600

    fix: homepage responsive on md - popular-sect

--- src/components/homePage/Promotional.jsx
+++ src/components/homePage/Promotional.jsx
@@ -30,10 +30,10 @@ const Promotional = () => {
       className=" bg-gradient-to-b from-white to-blue-100 md:py-32 py-5">
       <div className="container mx-auto text-center p-5 md:p-0 lg:pb-5">
         <div className=" relative">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold my-5">
+          <h1 className="text-secondary text-3xl md:text-4xl font-bold my-5">
             Sign up for newsletter
           </h1>
-          <p className="my-4 md:w-[40%] mx-auto">
+          <p className="my-4 md:w-[40%] mx-auto text-gray-600">
             Subscribe to our newsletter and stay informed about upcoming
             courses, events, and personalized recommendations for your learning
             journey.
@@ -60,16 +60,14 @@ const Promotional = () => {
         </div>
         <form
           onSubmit={handleSubmitNewsletter}
-          className="flex items-center justify-center gap-5 ">
-          <div className=" flex items-center gap-1">
+          className="flex justify-center gap-5 ">
             <input
-              className="input bg-[#DBE9FE] border focus:outline-none"
+              className="input bg-[#DBE9FE] border focus:outline-none shadow-md"
               type="email"
               required
               placeholder="Your Email"
             />
-          </div>
-          <button type="submit" className="btn bg-primary text-white">
+          <button type="submit" className="px-4 py-2 rounded-md bg-secondary text-white shadow-md transition-opacity hover:opacity-80">
             Subscribe
           </button>
         </form>
