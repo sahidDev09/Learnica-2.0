53877d7edff5a98dea5701c40c5bd789afbb1dad
rahatfaruk <rahatfaruk99@gmail.com>
2024-10-25 19:04:42 +0600

    update: LiveClass UI minor

--- src/app/live_class/LiveClasses.jsx
+++ src/app/live_class/LiveClasses.jsx
@@ -21,8 +21,8 @@ const LiveClasses = () => {
   }
 
   return (
-    <div className="p-4">
-      <h1 className="text-3xl md:text-4xl font-bold mb-8 mt-4 text-center text-secondary">Live Classes</h1>
+    <div className="p-4 mb-8 mt-4">
+      <h1 className="text-3xl md:text-4xl font-bold mb-8 text-center text-secondary">Live Classes</h1>
 
       {/* Responsive Grid Layout */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
