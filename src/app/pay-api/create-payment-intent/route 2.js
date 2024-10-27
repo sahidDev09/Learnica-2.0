035bb17152a136d81efa563587e0896be973c42c
sahidDev09 <iamsahidofficial99@gmail.com>
2024-10-28 00:12:51 +0600

    fix

---
+++ src/app/pay-api/create-payment-intent/route 2.js
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export async function POST(request) {
+  try {
+    const { amount, userId, email, items } = await request.json();
+
+    if (!amount || amount <= 0) {
+      return NextResponse.json({ success: false, message: "Invalid amount" }, { status: 400 });
+    }
+    const simplifiedItems = items.map(item => item.concept_title).join(', ');
+
+  
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(amount * 100), 
+      currency: "usd",
+      automatic_payment_methods: { enabled: true },
+      metadata: {
+        userId,
+        email,
+        items: simplifiedItems,
+      },
+    });
+   
+    return NextResponse.json({
+      success: true,
+      clientSecret: paymentIntent.client_secret,
+      paymentIntent, 
+    }, { status: 201 });
+
+  } catch (error) {
+    console.error("Error creating payment intent:", error);
+    return NextResponse.json({ success: false, message: "Server error" }, { status: 500 });
+  }
+}
