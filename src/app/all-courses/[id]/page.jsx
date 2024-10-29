fc624b4e545a9de4e54db1175b6f2cd7c2d111b7
farha12345678 <sabikunnaharfarha01@gmail.com>
2024-10-29 10:31:13 +0600

    data added

--- src/app/all-courses/[id]/page.jsx
+++ src/app/all-courses/[id]/page.jsx
@@ -296,7 +296,106 @@ useEffect(() => {
             allowFullScreen></iframe>
           {/* tab */}
 
-        
+          <div
+            role="tablist"
+            className="tabs tabs-bordered mt-4 bg-secondary pt-4 rounded-md w-full sm:max-w-none md:max-w-none lg:max-w-full flex flex-col md:inline-grid">
+            {/*--------------------------------- Overview --------------------------------*/}
+            <input
+              type="radio"
+              name="my_tabs_1"
+              className="tab mx-2 px-2 text-white"
+              aria-label="Overview"
+              defaultChecked
+            />
+            <div
+              role="tabpanel"
+              className="tab-content py-4 min-h-full bg-white w-full">
+              <h2 className="text-lg md:text-xl font-semibold">{data.title}</h2>
+              <div className="flex gap-5 my-4 w-full">
+                <div className="text-center">
+                  <div className="flex gap-2">
+                    <span className="font-semibold text-xl">4.6</span>{" "}
+                    <FaStar className="text-xl mt-1 text-orange-600" />
+                  </div>
+                  <h4 className="text-gray-400 font-semibold">
+                    34,506 ratings
+                  </h4>
+                </div>
+                <div className="text-center">
+                  <div className="font-semibold text-xl">5,406</div>
+                  <h4 className="text-gray-400 font-semibold">Students</h4>
+                </div>
+                <div className="text-center">
+                  <div className="font-semibold text-xl">50 h</div>
+                  <h4 className="text-gray-400 font-semibold">total</h4>
+                </div>
+              </div>
+
+              <h4 className="my-4">Last update 2024</h4>
+
+              {/* author card */}
+              <h4 className="text-secondary mb-2">Author</h4>
+              <div className="flex gap-2 items-center p-2 bg-card rounded-md">
+                <Image
+                  width={30}
+                  height={30}
+                  src={"/assets/developers/numan.jpg"}
+                  alt="video_thumbnail"
+                  className="rounded w-16 h-16"></Image>
+                <div className="text-start">
+                  <h2 className="text-lg md:text-xl font-semibold">Jhon doe</h2>
+                  <h4 className="text-gray-500">Web Developer </h4>
+                </div>
+              </div>
+            </div>
+            {/* --------------------------- Q&A -------------------------*/}
+            <input
+              type="radio"
+              name="my_tabs_1"
+              className="tab mx-2 px-2 text-white"
+              aria-label="Q&A"
+            />
+            <div role="tabpanel" className="tab-content py-4 bg-white w-full">
+              <Questions courseId={courseId} />
+            </div>
+            {/*------------------------------- Notes ----------------------------*/}
+            <input
+              type="radio"
+              name="my_tabs_1"
+              role="tab"
+              className="tab mx-2 px-2 text-white"
+              aria-label="Notes"
+            />
+            <div role="tabpanel" className="tab-content py-2 bg-white w-full">
+              <AddNoteForm courseId={courseId} />
+              <Notes courseId={courseId} />
+            </div>
+            {/*------------------------------- Reviews --------------------------*/}
+            <input
+              type="radio"
+              name="my_tabs_1"
+              role="tab"
+              className="tab mx-2 px-2 text-white"
+              aria-label="Reviews"
+            />
+            <div role="tabpanel" className="tab-content pt-4 bg-white w-full">
+              <MyReview courseId={courseId} />
+              <Reviews courseId={courseId} />
+            </div>
+
+            {/*------------------------------- Attachment --------------------------*/}
+
+            <input
+              type="radio"
+              name="my_tabs_1"
+              role="tab"
+              className="tab mx-2 px-2 text-white"
+              aria-label="Resources"
+            />
+            <div role="tabpanel" className="tab-content bg-white pt-2 w-full">
+              <Resources courseId={data?._id} userid={data?.author?.id} />
+            </div>
+          </div>
         </div>
       </div>
 
