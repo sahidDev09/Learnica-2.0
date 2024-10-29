3cf0ce3fa368ae24ddc2c2b9be98a8774f9d7f11
rahatfaruk <rahatfaruk99@gmail.com>
2024-10-29 18:30:34 +0600

    add: my-learning > custom-courses UI

--- src/app/my-learning/CustomCoursesContent.jsx
+++ src/app/my-learning/CustomCoursesContent.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import Loader from "@/components/shared/Loader";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 function CustomCoursesContent({ user }) {
   const userEmail = user?.emailAddresses[0]?.emailAddress;
@@ -16,14 +18,30 @@ function CustomCoursesContent({ user }) {
     },
   });
 
-  if (isLoading) { return <Loader /> }  
+  if (isLoading) { return <Loader /> }
   return (
     <div className="border-2 rounded-md mt-3 px-4 pb-6 pt-4">
       <h3 className="text-xl md:text-2xl font-semibold mb-6 text-secondary">My Custom Courses</h3>
 
       {customCourses.length > 0 ? (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-          custom courses cards
+          {customCourses?.map(course => (
+            <div key={course._id} className="bg-card p-6 rounded-md border-2 border-secondary">
+              <h3 className="text-lg font-semibold mb-1 text-secondary">
+                {course.title.length > 35
+                  ? `${course.title.slice(0, 35)}...`
+                  : course.title}
+              </h3>
+              <p className="text-sm text-gray-500 mt-1">
+                Total class: <span className="font-semibold">{course.items?.length}</span>
+              </p>
+              <div className="flex items-center justify-between gap-4">
+                <Link href={`/`}>
+                  <Button className="mt-4 mb-2 bg-secondary">Continue</Button>
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
       ) : (
         <p className="text-xl md:text-2xl p-6 pl-0 font-semibold text-gray-500">No custom courses have been bought yet!</p>
