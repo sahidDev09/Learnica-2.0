a14a5a498ce8d020d20f6fdcb7443d4b5613aaf3
rahatfaruk <rahatfaruk99@gmail.com>
2024-10-29 18:12:05 +0600

    update: my-learing api -> get custom-courses

--- src/app/my-learning/CustomCoursesContent.jsx
+++ src/app/my-learning/CustomCoursesContent.jsx
@@ -5,28 +5,28 @@ function CustomCoursesContent({ user }) {
   const userEmail = user?.emailAddresses[0]?.emailAddress;
 
   // get notes
-  const { data: courses, isLoading } = useQuery({
+  const { data: customCourses, isLoading } = useQuery({
     queryKey: ["my-custom-courses"],
     queryFn: async () => {
       const res = await fetch(
         process.env.NEXT_PUBLIC_BASE_URL +
-        `/api/my-learning?email=${userEmail}?type=custom`
+        `/api/my-learning?email=${userEmail}&type=custom`
       );
       return res.json();
     },
   });
 
-  if (isLoading) { return <Loader /> }
+  if (isLoading) { return <Loader /> }  
   return (
     <div className="border-2 rounded-md mt-3 px-4 pb-6 pt-4">
       <h3 className="text-xl md:text-2xl font-semibold mb-6 text-secondary">My Custom Courses</h3>
 
-      {courses.length > 0 ? (
+      {customCourses.length > 0 ? (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
           custom courses cards
         </div>
       ) : (
-        <p className="text-xl md:text-2xl p-6 font-semibold text-gray-500">No custom courses have been bought yet!</p>
+        <p className="text-xl md:text-2xl p-6 pl-0 font-semibold text-gray-500">No custom courses have been bought yet!</p>
       )}
     </div>
   );
--- src/pages/api/my-learning.js
+++ src/pages/api/my-learning.js
@@ -11,19 +11,31 @@ export default async function handler(req, res) {
 
     if (req.method === "GET") {
       const email = req.query?.email
-      // > get orders based on email
-      const ordersArr = await ordersCollection.find(
-        {email, type: "course"}, 
-        {projection: {_id: 0, courseId: 1}}
-      ).toArray();
-      // > get courses forEach order's courseId
-      const coursesIds = ordersArr.map(order => new ObjectId(order.courseId))
-      const courses = await coursesCollection.find(
-        {_id: {$in: coursesIds}}, 
-        {projection: {additionalInfo: 1, name: 1, category: 1, publish_date:1, author:1, pricing:1}}
-      ).toArray();
+      const type = req.query?.type
+      let result;
 
-      return res.json(courses);
+      if (type === "custom") {        
+        result = await ordersCollection.find(
+          {email, type}, 
+          {projection: {title: 1, items: 1}}
+        ).toArray();        
+      } 
+      // type courses
+      else {
+        // > get orders based on email
+        const ordersArr = await ordersCollection.find(
+          {email, type: "course"}, 
+          {projection: {_id: 0, courseId: 1}}
+        ).toArray();
+        // > get courses forEach order's courseId
+        const coursesIds = ordersArr.map(order => new ObjectId(order.courseId))
+        result = await coursesCollection.find(
+          {_id: {$in: coursesIds}}, 
+          {projection: {additionalInfo: 1, name: 1, category: 1, publish_date:1, author:1, pricing:1}}
+        ).toArray();
+      }
+
+      return res.json(result);
     } 
     else {
       // Handle unsupported HTTP methods
